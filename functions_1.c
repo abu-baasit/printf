8c849a75e8708b3c606b6f477efc759e6de4f890
marylambtech <marylambtech@gmail.com>
2023-07-25 21:48:56 -0700

    betty corrections

--- functions_1.c
+++ functions_1.c
@@ -2,26 +2,27 @@
 #include <stdarg.h>
 
 /**
-* write_interger -Function to write an integer to the standard output
+* write_integer - Function to write an integer to the standard output
 * @num : integer given
-* @buffer: buffer
-* @len : length
 * Return:#include "main.h"
-
 */
-void write_integer(int num) {
-    char buffer[20];
-    int len = 0;
 
-    do {
-        buffer[len++] = '0' + (num % 10);
-        num /= 10;
-    } while (num != 0);
-
-    while (len > 0) {
-        len--;
-        write(1, &buffer[len], 1);
-    }
+void write_integer(int num)
+{
+char buffer[20];
+int len = 0;
+do {
+buffer[len++] = '0' + (num % 10);
+num /= 10;
+}
+while (num != 0)
+{
+while (len > 0)
+{
+len--;
+write(1, &buffer[len], 1);
+}
+}
 }
 
 /**
@@ -29,28 +30,31 @@ void write_integer(int num) {
 * @format:parameter
 * Return:return argument
 */
-void custom_printf(const char* format, ...)
- {
-    va_list list_args;
-    va_start(list_args, format);
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-
-            if (*format == 'd' || *format == 'i') {
-                int number = va_arg(list_args, int);
-                write_integer(number);
-            } else {
-               
-                write(1, format - 1, 1);
-            }
-        } else {
-           
-            write(1, format, 1);
-        }
-        format++;
-    }
-
-    va_end(list_args);
+void custom_printf(const char *format, ...)
+{
+va_list list_args;
+va_start(list_args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'd' || *format == 'i')
+{
+int number = va_arg(list_args, int);
+write_integer(number);
+}
+else
+{
+write(1, format - 1, 1);
+}
+}
+else
+{
+write(1, format, 1);
+}
+format++;
+}
+va_end(list_args);
 }
